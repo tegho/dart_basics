855994f705de5e5bc1cf484fb6a4eaf281f36388
vovka <v-dorokhin@yandex.ru>
2022-11-03 03:24:08 +0300

    nod-nok

--- main.dart
+++ main.dart
@@ -3,4 +3,15 @@ import 'package:my01_flutter/nod_nok.dart';
 
 void main() {
   // runApp(const MyApp());
+  NodNok nn;
+  nn = NodNok( [0,0] );
+  nn = NodNok( [1,0] );
+  nn = NodNok( [10,-5] );
+  nn = NodNok( [3,-6] );
+  nn = NodNok( [-3,-4] );
+  nn = NodNok( [63,4] );
+  nn = NodNok( [10946, 17711] );
+  nn = NodNok( [4096, 64,-32,1024,128] );
+  nn = NodNok.pair( 4096, 128 );
+
 }
---
+++ nod_nok.dart
@@ -0,0 +1,55 @@
+/// Class to find Nod and Nok for any number of integers
+class NodNok {
+  int nod = 1;
+  int nok = 0;
+
+  // nod for two integers
+  int _nodPair(int a, int b) {
+
+    if ( a < b ) {
+      int t = b;
+      b = a;
+      a = t;
+    }
+
+    while ( b != 0 ) {
+      a %= b;
+      int t = b;
+      b = a;
+      a = t;
+    }
+
+    // Nod:
+    if ( a == 0 ) a = 1;
+    if ( a < 0 ) a *= -1;
+    return a ;
+  }
+
+  // nok for two integers
+  int _nokPair(int a, int b, int n) {
+
+    int res = a ~/ n * b;
+    if ( res < 0 ) res *= -1;
+    return res;
+  }
+
+  /// Constructor for list of integers
+  NodNok(List numbers) {
+    if (numbers.length < 2) throw ArgumentError.value(numbers.length, 'numbers', 'There must be at least two numbers');
+    for (var n in numbers) {
+      if (n is! int) throw ArgumentError.value(n, 'numbers', 'Only integers allowed');
+    }
+
+    nod = _nodPair(numbers[0], numbers[1]);
+    nok = _nokPair(numbers[0], numbers[1], nod);
+    for (var i = 2; i < numbers.length; i++ ) {
+      nod = _nodPair(nod, numbers[i]);
+      nok = _nokPair(nok, numbers[i], _nodPair(nok, numbers[i]));
+    }
+    // print(numbers);
+    // print('NOD=$nod  NOK=$nok');
+  }
+
+  /// Constructor for two integers
+  NodNok.pair(int a, int b) : this([a, b]);
+}
